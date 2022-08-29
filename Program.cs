fb7d6ef8bec950ccadbceb3c7e42c6fa1300600d
Spiridonova Victoria <spiridonowa.viky@yandex.ru>
2022-08-29 17:11:02 +0300

    нов

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите цифру, соответствующую дню недели:");
+int number = int.Parse(Console.ReadLine());
+
+while (number<1 || number >7)
+{
+    Console.WriteLine("Цифра не соответствует никакому дню недели. Введите цифру, соответствующую дню недели:");
+    number = int.Parse(Console.ReadLine());
+}
+
+if (number>0 && number <6)
+{
+    Console.WriteLine("Рабочий день");
+}
+else
+{
+    Console.WriteLine("Выходной день");
+}
