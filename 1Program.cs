25602dfde6eb74284be7505b04e372297329beb7
SpiridonowaViky <110791518+SpiridonowaViky@users.noreply.github.com>
2022-09-03 02:03:46 +0300

    new

---
+++ 1Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число A");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B = " + step);
---
+++ 2Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int i = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("Сумма цифр в числе = " + sum);
